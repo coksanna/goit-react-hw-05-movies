8ffab558f5a74b073fbf5d4f7d5b13f487e4155d
Oksana Kyryliuk <coksanna@gmail.com>
2023-02-17 14:30:46 +0200

    in progress...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,39 @@
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
-import UserRoutes from './UserRoutes';
 
-// import './App.css';
+import { lazy, Suspense } from 'react';
 
-function App() {
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const SingleMoviePage = lazy(() =>
+  import('../pages/SingleMoviePage/SingleMoviePage')
+);
+const SingleMoviePageCast = lazy(() =>
+  import('../pages/SingleMoviePageCast/SingleMoviePageCast')
+);
+const SingleMoviePageReviews = lazy(() =>
+  import('../pages/SingleMoviePageReview/SingleMoviePageReview')
+);
+const MovieSearchPage = lazy(() =>
+  import('../pages/MovieSearchPage/MovieSearchPage')
+);
+
+export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <Navbar />
-      <UserRoutes />
+      <Suspense fallback={<p>...loading</p>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie-search" element={<MovieSearchPage />} />
+          <Route path="/movie/:id" element={<SingleMoviePage />}>
+            <Route path="cast" element={<SingleMoviePageCast />} />
+            <Route path="reviews" element={<SingleMoviePageReviews />} />
+          </Route>
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
